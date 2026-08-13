35d14932bb6d143eda63c847853891aec121a6be
Rent-Home <ajaywavare1996@gmail.com>
2026-08-13 23:11:55 +0530

    Update main.jsx
    this is for 404 error when reload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
@@ -10,12 +10,12 @@ import { AdminAuthProvider } from "./context/AdminAuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
          <AdminAuthProvider>
       <App />
     </AdminAuthProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
